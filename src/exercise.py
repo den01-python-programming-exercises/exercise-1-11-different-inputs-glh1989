be47dcdaa0a0bbaa17bd8b8750f626e6c1943169
gh1989 <hughesg368@hwbcymru.net>
2021-03-31 12:02:49 +0000

    attempt solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,18 @@
 def main():
-    #write your code below this line
+    string = input("Give a String ")
+    string_as_int = input("Give an Integer ")
+    string_as_float = input("Give a float ")
+    string_as_boolean = input("Give a Boolean ")
 
+    intvalue = int(string_as_int)
+    floatvalue = float(string_as_float)
+    booleanvalue = bool(string_as_boolean)
+
+
+    print("You gave the string " + string)
+    print("You gave the integer " + str(intvalue))
+    print("You gave the float " + str(floatvalue))
+    print("You gave the boolean " + str(booleanvalue))
+    
 if __name__ == '__main__':
     main()
